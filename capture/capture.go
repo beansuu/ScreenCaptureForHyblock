2dee551a48465b9f38993ecd0ccd40a2c836107c
mtalli <mauno.talli@hotmail.com>
2023-08-07 05:36:47 +0300

    Trying sound alarm

--- capture/capture.go
+++ capture/capture.go
@@ -5,23 +5,14 @@ import (
 	"image"
 	"image/color"
 	"log"
-	"net/smtp"
 	"time"
 
+	"github.com/gen2brain/beeep"
 	"github.com/kbinani/screenshot"
 )
 
 func main() {
 	targetColor := color.RGBA{R: 0x9c, G: 0x27, B: 0xb0, A: 0xff}
-	emailAddress := "elaroldwolf@hotmail.com"
-	smtpServer := "smtp.office365.com"
-	smtpPort := 587
-	smtpUsername := "elaroldwolf@hotmail.com"
-	smtpPassword := "keerul1ne"
-
-    sendEmailNotification(emailAddress, smtpServer, smtpPort, smtpUsername, smtpPassword)
-
-	var previousScreen image.Image
 
 	for {
 		screenBounds := screenshot.GetDisplayBounds(0)
@@ -31,25 +22,23 @@ func main() {
 			continue
 		}
 
-		if previousScreen != nil && isColorChange(previousScreen, img, targetColor) {
-			fmt.Println("New purple color detected!")
-			sendEmailNotification(emailAddress, smtpServer, smtpPort, smtpUsername, smtpPassword)
+		found := isColorFound(img, targetColor)
+		if found {
+			fmt.Println("Target color found!")
+			displayDesktopNotification("Purple Color Detected", "Purple color was detected on the screen!")
+		} else {
+			fmt.Println("Target color not found.")
 		}
-
-		previousScreen = img
 
 		time.Sleep(5 * time.Second) // Wait for 5 seconds before capturing the next screenshot
 	}
 }
 
-func isColorChange(prevScreen, currentScreen image.Image, targetColor color.Color) bool {
-	bounds := prevScreen.Bounds()
+func isColorFound(img image.Image, targetColor color.Color) bool {
+	bounds := img.Bounds()
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-			prevColor := prevScreen.At(x, y)
-			currColor := currentScreen.At(x, y)
-
-			if prevColor != currColor && currColor == targetColor {
+			if img.At(x, y) == targetColor {
 				return true
 			}
 		}
@@ -57,16 +46,9 @@ func isColorChange(prevScreen, currentScreen image.Image, targetColor color.Colo
 	return false
 }
 
-func sendEmailNotification(emailAddress, smtpServer string, smtpPort int, smtpUsername, smtpPassword string) {
-    auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpServer)
-    to := []string{emailAddress}
-    msg := []byte("To: " + emailAddress + "\r\n" +
-        "Subject: Purple Color Detected\r\n" +
-        "\r\n" +
-        "Purple color was detected on the screen!\r\n")
-
-    err := smtp.SendMail(smtpServer+":"+fmt.Sprintf("%d", smtpPort), auth, smtpUsername, to, msg)
-    if err != nil {
-        log.Println("Error sending email:", err)
-    }
+func displayDesktopNotification(title, message string) {
+	err := beeep.Notify(title, message, "")
+	if err != nil {
+		log.Println("Error displaying desktop notification:", err)
+	}
 }
--- capture/go.mod
+++ capture/go.mod
@@ -3,10 +3,15 @@ module capture.go
 go 1.20
 
 require (
+	github.com/gen2brain/beeep v0.0.0-20230602101333-f384c29b62dd // indirect
 	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
 	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
